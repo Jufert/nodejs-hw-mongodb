4a1e3726d1fe2abbe3f968cba7490a6061091377
Jufert <mr.jufert@gmail.com>
2024-09-07 07:08:38 +0200

    pull

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,18 @@
+import { isHttpError } from 'http-errors';
+
+export async function errorHandler(error, req, res, next) {
+  if (isHttpError(error) === true) {
+    return res.status(error.status).send({
+      status: error.status,
+      message: error.message,
+    });
+  }
+
+  res
+    .status(500)
+    .send({
+      status: 500,
+      message: 'Something went wrong',
+      data: error.message,
+    });
+}
